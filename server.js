a397150bdcbb48cef6dbfa2d38dfb1342cc3a118
DESKTOP-T90CO9O\Administrator <meijh@fosun.com>
2019-09-05 16:59:47 +0800

    test

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-let http = require('http');
-let users = [
-    {id:11,name:'zhufeng1'},
-    {id:22,name:'zhufeng2'},
-    {id:33,name:'zhufeng3'}
-]
-let server = http.createServer(function(req,res){
-  res.setHeader('Access-Control-Allow-Origin','*');
-  if(req.url === '/api/users'){
-      res.end(JSON.stringify(users));
-  }else{
-      res.end('Not Found');
+let http = require('http')
+
+let users = [{ id: 666, name: '张三' }, { id: 888, name: '李四' }, { id: 999, name: '王五' }]
+let server = http.createServer(function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  if (req.url === '/api/users') {
+    res.end(JSON.stringify(users))
+  } else {
+    res.end('Not Found')
   }
-});
-server.listen(3000,()=>{
-    console.log('后端API接口服务器已经启动在3000端口！')
-});
+})
+server.listen(3000, () => {
+  console.log('后端API接口服务器已经启动在3000端口！')
+})
