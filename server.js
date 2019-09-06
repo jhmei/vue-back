4e63785ad324dbe97785eff894dc3ee7afd2862a
DESKTOP-T90CO9O\Administrator <meijh@fosun.com>
2019-09-06 10:25:57 +0800

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let http = require('http')
 
-let users = [{ id: 66, name: '张三' }, { id: 88, name: '李四' }, { id: 99, name: '王五' }]
+let users = [{ id: 6, name: '张三' }, { id: 7, name: '李四' }, { id: 8, name: '王五' }]
 let server = http.createServer(function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   if (req.url === '/api/users') {
