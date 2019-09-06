f6e2a6ccb4cf8299430c8663fa6f7087c51acac6
DESKTOP-T90CO9O\Administrator <meijh@fosun.com>
2019-09-06 12:59:29 +0800

    test

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let http = require('http')
 
-let users = [{ id: 11, name: '张三' }, { id: 22, name: '李四' }, { id: 33, name: '王五' }]
+let users = [{ id: 11, name: 'zhangsan' }, { id: 22, name: 'lisi' }, { id: 33, name: 'wangwu' }]
 let server = http.createServer(function(req, res) {
-  res.setHeader('Content-Type', 'text/plain;charset=utf-8') // 解决乱码
+  res.setHeader('Content-Type', 'text/plain;charset=utf-8') // 解决中文乱码
   res.setHeader('Access-Control-Allow-Origin', '*')
   if (req.url === '/api/users') {
     res.end(JSON.stringify(users))
