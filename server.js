fc3972424024a7b6d762f02209b3e55fcdb9bbe9
DESKTOP-T90CO9O\Administrator <meijh@fosun.com>
2019-09-11 13:28:38 +0800

    test

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-let http = require('http')
+const http = require('http')
 
-let users = [{ id: 8, name: '请问' }, { id: 88, name: '以为' }, { id: 888, name: '认为' }]
-let server = http.createServer(function(req, res) {
+const list = [
+  { id: 8, name: '请问' },
+  { id: 88, name: '以为' },
+  { id: 888, name: '认为' }
+]
+
+const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain;charset=utf-8') // 解决中文乱码
   res.setHeader('Access-Control-Allow-Origin', '*')
-  if (req.url === '/api/users') {
-    res.end(JSON.stringify(users))
+  if (req.url === '/api/list') {
+    res.end(JSON.stringify(list))
   } else {
     res.end('Not Found')
   }
 })
+
 server.listen(3000, () => {
-  console.log('后端API接口服务器已经启动在3000端口了')
+  console.log('后端服务已经启动在3000端口')
 })
